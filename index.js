6763715431f0d866b48c32054a705a0fcbaf2faf
George Bora <gpbora@gmail.com>
2022-10-03 01:30:53 +0300

    fixed

--- index.js
+++ index.js
@@ -14,45 +14,27 @@ const port = configs.port;
 app.get("/users", async (req, res) => {
 	try {
 		const results = await usersRepo.getAllUsers();
-		res.status(200).send(results)
+		res.status(200).send(results);
 	} catch(e) {
 		console.error(e);
 		res.sendStatus(500);
 	}
 });
 
-app.get("/users/:id", (req, res) => {
-  const user = ITEMS.users.find(
-    (user) => {
-		return user.id === req.params.id
-	}
-  );
-  if (user) {
-    return sendResponse(res.status(200), user);
-  }
-  return sendResponse(res.status(404), null);
+app.get("/users/:id", async (req, res) => {
+	console.log(req.params.id);
 });
 
-app.post("/users", (req, res) => {
-  const body = req.body || {};
-  ITEMS.users.push({...body, id: crypto.randomUUID() });
-  return sendResponse(res.status(201), ITEMS.users[ITEMS.users.length - 1]);
+app.post("/users", async (req, res) => {
+	console.log(req.body);
 });
 
-app.put("/users/:id", (req, res) => {
-  const body = req.body || {};
-  const userIdx = ITEMS.users.findIndex(
-    (user) => user.id === req.params.id
-  );
-  const user = ITEMS.users[userIdx];
-  ITEMS.users[userIdx] = { ...body }
-  return sendResponse(res.status(201), ITEMS.users[userIdx]);
+app.put("/users/:id", async (req, res) => {
+	console.log(req.params.id, req.body)
 });
 
-app.delete("/users/:id", (req, res) => {
-  const id = req.params.id
-  ITEMS.users = ITEMS.users.filter(user => user.id !== id)
-  return sendResponse(res.status(200))
+app.delete("/users/:id", async (req, res) => {
+	console.log(req.params.id);
 });
 
 app.listen(port, () => {
--- users/config.repository.js
+++ users/config.repository.js
@@ -1,14 +1,14 @@
 const { MongoClient } = require("mongodb");
 const configs = require("../configs");
 
-
+const client = new MongoClient(configs.databaseURL);
 
 const getAllUsers = async () => {
-    let client = new MongoClient(configs.databaseURL);
     await client.connect();
-    collection = client.db("gym_users").collection("users");
-    let cursor = collection.find();
-    let result = cursor.toArray();
+    let gymUsersDatabase = client.db("gym_users");
+    let usersCollection = gymUsersDatabase.collection("users");
+    let cursor = await usersCollection.find();
+    let result = await cursor.toArray();
     await client.close();
     return result;
 }
